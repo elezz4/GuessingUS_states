c032700d4762dace09c48dbe85d402b36b8a664a
Amel Elezović <elezovicamel267@gmail.com>
2026-03-02 23:06:00 +0100

    Rename Timer class to TimeBoard and adjust time handling
    Refactor Timer class to TimeBoard and update time display logic.

--- timer.py
+++ timer.py
@@ -1,26 +1,26 @@
-from functools import update_wrapper
-from turtle import Turtle, Screen
-import time
+from turtle import Turtle
 
-class Timer(Turtle):
+class TimeBoard(Turtle):
     def __init__(self, screen, game_over):
         super().__init__()
         self.hideturtle()
         self.penup()
-        self.screen = screen
+        self.color("black")
         self.game_over = game_over
-        self.time = 10
-        self.goto(-280, 250)
-        self.update_score()
+        self.screen = screen
+        self.time = 600
+        self.print_time_score()
 
-    def update_score(self):
+
+    def print_time_score(self):
         self.clear()
-        self.write(arg=f"Remaining time: {self.time}", move=False, font=("Montserat", 17, "normal"))
-        if self.time > 0:
+        self.goto(-300, 300)
+        minutes = self.time // 60
+        seconds = self.time % 60
+        self.write(arg=f"TIME LEFT: {minutes:02d}:{seconds:02d}", move=False, align="center", font=("Arial", 15, "bold"))
+
+        if self.time > 0 and self.game_over == False:
             self.time -= 1
-            self.screen.ontimer(self.update_score, 1000)
+            self.screen.ontimer(self.print_time_score, 1000)
         else:
             self.game_over = True
-
-
-
